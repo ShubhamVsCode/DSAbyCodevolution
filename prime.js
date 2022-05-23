3b768f5a35393823591d2a807cb063b3c84471a3
ShubhamVsCode <shubhamvscode@gmail.com>
2022-05-23 22:13:06 +0530

    1 file modified

--- prime.js
+++ prime.js
@@ -1,7 +1,13 @@
 function prime(n) {
-  for (let i = 2; i < n; i++) {
+  if (n < 2) return false;
+
+  for (let i = 2; i < Math.sqrt(n); i++) {
     console.log(` ${n} % ${i} = ${n % i}`);
+    if (n % i == 0) {
+      return false;
+    }
   }
+  return true;
 }
 
-console.log(prime(7)); // true
+console.log(prime(47)); // true
